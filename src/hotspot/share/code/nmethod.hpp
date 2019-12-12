graalvm/graal-jvmci-8

8229961: Assert failure in compiler/graalunit/HotspotTest.java

src/share/vm/code/nmethod.hpp
@@ -864,10 +864,10 @@ class nmethodLocker : public StackObj {
   ~nmethodLocker() { unlock_nmethod(_nm); }
 
   nmethod* code() { return _nm; }
-  void set_code(nmethod* new_nm) {
+  void set_code(nmethod* new_nm, bool zombie_ok = false) {
     unlock_nmethod(_nm);   // note:  This works even if _nm==new_nm.
     _nm = new_nm;
-    lock_nmethod(_nm);
+    lock_nmethod(_nm, zombie_ok);
   }
 };
 

graalvm/labs-openjdk-11

src/hotspot/share/code/nmethod.hpp
@@ -627,9 +627,9 @@ class nmethodLocker : public StackObj {
     lock(_nm);
   }
 
-  static void lock(CompiledMethod* method) {
+  static void lock(CompiledMethod* method, bool zombie_ok = false) {
     if (method == NULL) return;
-    lock_nmethod(method);
+    lock_nmethod(method, zombie_ok);
   }
 
   static void unlock(CompiledMethod* method) {
@@ -643,10 +643,10 @@ class nmethodLocker : public StackObj {
   }
 
   CompiledMethod* code() { return _nm; }
-  void set_code(CompiledMethod* new_nm) {
+  void set_code(CompiledMethod* new_nm, bool zombie_ok = false) {
     unlock(_nm);   // note:  This works even if _nm==new_nm.
     _nm = new_nm;
-    lock(_nm);
+    lock(_nm, zombie_ok);
   }
 };
 