3da5b089f312a5b2efa0c9f09b76f4cea86fe572
Tom Rodriguez <tom.rodriguez@oracle.com>
2020-01-22 09:52:18 -0800

    Sync with jdk14 version

--- src/hotspot/share/classfile/javaClasses.cpp
+++ src/hotspot/share/classfile/javaClasses.cpp
@@ -2653,7 +2653,8 @@ void java_lang_StackTraceElement::decode_file_and_line(Handle java_class, Instan
 }
 
 #if INCLUDE_JVMCI
-void java_lang_StackTraceElement::decode(methodHandle method, int bci, Symbol*& methodname, Symbol*& filename, int& line_number, TRAPS) {
+void java_lang_StackTraceElement::decode(const methodHandle& method, int bci,
+                                         Symbol*& filename, int& line_number, TRAPS) {
   ResourceMark rm(THREAD);
   HandleMark hm(THREAD);
 
@@ -2665,8 +2666,6 @@ void java_lang_StackTraceElement::decode(methodHandle method, int bci, Symbol*& 
   InstanceKlass* holder = method->method_holder();
   Handle java_class(THREAD, holder->java_mirror());
   decode_file_and_line(java_class, holder, version, method, bci, filename, source_file, line_number, CHECK);
-
-  methodname = method->name();
 }
 #endif // INCLUDE_JVMCI
 
