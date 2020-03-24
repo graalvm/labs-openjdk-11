eedbc8181573fb3587b602a00f411ca29ed41fee
Tom Rodriguez <tom.rodriguez@oracle.com>
2020-03-24 01:01:19 +0000

    [GR-21527] Export CodeOffsets::Frame_Complete in JVMCI.
    PullRequest: labsjdk-ce-11/67

--- src/hotspot/share/jfr/periodic/sampling/jfrCallTrace.hpp
+++ src/hotspot/share/jfr/periodic/sampling/jfrCallTrace.hpp
@@ -40,6 +40,8 @@ class JfrGetCallTrace : public StackObj {
   JfrGetCallTrace(bool in_java, JavaThread* thread) : _in_java(in_java), _thread(thread) {}
   bool find_top_frame(frame& topframe, Method** method, frame& first_frame);
   bool get_topframe(void* ucontext, frame& top);
+
+  static void print_statistics();
 };
 
 #endif // SHARE_VM_JFR_ENGINE_SAMPLING_JFRCALLTRACE_HPP
