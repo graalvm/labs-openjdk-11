f70483cd8a1038c7fc35ef6d9441066329830ac7
taha alamine boukhalf <taha.alamine.boukhalf@oracle.com>
2022-02-17 15:15:07 +0100

    update JVMCI_VM_ENTRY_MARK

--- src/hotspot/share/jvmci/jvmciCompilerToVM.cpp
+++ src/hotspot/share/jvmci/jvmciCompilerToVM.cpp
@@ -119,6 +119,7 @@ Handle JavaArgumentUnboxer::next_arg(BasicType expectedType) {
 
 // Bring the JVMCI compiler thread into the VM state.
 #define JVMCI_VM_ENTRY_MARK                   \
+  MACOS_AARCH64_ONLY(ThreadWXEnable __wx(WXWrite, thread)); \
   ThreadInVMfromNative __tiv(thread);         \
   ResetNoHandleMark rnhm;                     \
   HandleMarkCleaner __hm(thread);             \
