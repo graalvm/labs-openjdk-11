7c102c8ccf100137c4cb51fdc78009fac8251cc6
Marouane el hallaoui <marouane.el.hallaoui@oracle.com>
2022-05-23 09:58:24 +0100

    tmp

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -448,7 +448,7 @@ var getJibProfilesProfiles = function (input, common, data) {
             target_cpu: "aarch64",
             dependencies: ["devkit"],
             configure_args: concat(common.configure_args_64bit, "--with-zlib=system",
-                "--with-macosx-version-max=10.12.00"),
+                "--with-macosx-version-max=11.00.00"),
         },
 
         "solaris-x64": {
