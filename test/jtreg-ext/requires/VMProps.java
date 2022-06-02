8a0fac00c19d076c08b39dc696271a1379ffb96a
Aleksandar Gradinac <aleksandar.gradinac@oracle.com>
2022-06-02 17:58:27 +0200

    Cleanup remaining Portola changes as musl support has been merged upstream

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -24,11 +24,9 @@
 package requires;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
