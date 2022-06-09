11c0fc8d7aaa01974935b84127f1d24c904d997f
Taha Alamine Boukhalf <taha.alamine.boukhalf@oracle.com>
2022-06-09 15:21:44 +0100

    solve compilation issues

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -24,9 +24,11 @@
 package requires;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
